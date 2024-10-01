de30df85878499f9dbce1a1ef36c8606ae53bd5d
Shekhar2791 <rahulamerica12@gmail.com>
2024-10-01 16:18:28 +0530

    test

--- src/main/java/com/ex/Proj1SecurityApplication.java
+++ src/main/java/com/ex/Proj1SecurityApplication.java
@@ -3,10 +3,11 @@ package com.ex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.ex.entity")
-public class Proj1SecurityApplication {
+public class Proj1SecurityApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proj1SecurityApplication.class, args);
